2bfd7be63538dee9f9925b51e4abdaf20bc76354
pluzinc <132268195+pluzinc@users.noreply.github.com>
2023-11-21 02:09:18 -0500

    denied page design

--- app.js
+++ app.js
@@ -22,8 +22,8 @@ function workingHours(req, res, next) {
 
   const normalBusinessHours = {
     // 24 hour time
-    open: 9,
-    close: 17,
+    open: 10: 30,
+    close: 18: 30,
   };
 
   // check if within normal business hours
@@ -34,11 +34,11 @@ function workingHours(req, res, next) {
 
     // if so, point the request to our static files
     console.log('Open!');
-    req.url = 'chauncey-gardiner-resume.pdf';
+    req.url = 'Resume Samples.pdf';
     next();
 
   } else {
-    
+
     // otherwise, return the denial
     console.log('Closed 🔒');
     req.url = 'denied.html';
